6c90abd3b9d59fb4aac9fce9744f3538425edd74
[CarolinaCollao] <[carolinacollaopizarro@gmail.com]>
2023-03-01 17:13:05 -0300

    se agregó hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <p className=''>Subtítulo superior</p>
+            <p className=''>Bajada de texto </p>
+            <p className=''>Gran Título</p>
+            <p className=''>Bajada de texto </p>
+            <button href='/' className='button'>Más Información</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
