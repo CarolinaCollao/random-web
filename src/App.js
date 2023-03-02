cf863e779c3a6ebff219f6dfda35040da7e0e98f
[CarolinaCollao] <[carolinacollaopizarro@gmail.com]>
2023-03-02 17:53:19 -0300

    se agregó página 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Page1 from "./components/Page1";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Page1 />
     </div>
   );
 }
---
+++ src/components/Page1.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Page1.css'
+import retrato from '../images/retrato.png'
+
+const Page1 = () => {
+    return (
+        <div className='page1'>
+        <div className='container'>
+                <img src={retrato} alt='retrato' />
+                <div className='col-2'>
+                    <h2>Título</h2>
+                    <span className='line'></span>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi.</p>
+                    <p>Cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</p>
+                    <button className='button'>Ver más</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page1
