f2ba24c000bf4e0b66ff8d14ad5f8115bba85ed9
[CarolinaCollao] <[carolinacollaopizarro@gmail.com]>
2023-03-02 22:42:34 -0300

    se creo página 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Page1 />
       <Page2 />
+      <Page3 />
     </div>
   );
 }
---
+++ src/components/Page3.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Page3.css'
+
+const Page3 = () => {
+  return (
+    <div className='page3'>
+      <div className='container'>
+        <div className='col-1'>
+          <p>Excepteur sint occaecat</p>
+          <p>Fugiat nulla pariatur</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <button className='button'>Ver más</button>
+        </div>
+
+        <div className='col-2'>
+          <iframe width='570' height='320' src='https://www.youtube.com/watch?v=sw7463aYB5k' title='Youtube video player' frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page3
